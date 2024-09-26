23ccd83a2bd5a44ccfcce12ea678af0e860ed35e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding@users.noreply.github.com>
2024-09-26 12:33:29 -0600

    Create 01-hello-world.c

---
+++ portfolio/01-hello-world.c
@@ -0,0 +1,25 @@
+/**
+ * ==============================================================================
+ * Author: Michael Gene Brockus (Dreamer)
+ * Email: michaelbrockus@gmail.com
+ * Organization: Fossil Logic
+ * Description: 
+ *     This file is part of the Fossil Logic project, where innovation meets
+ *     excellence in software development. Michael Gene Brockus, also known as
+ *     "Dreamer," is a dedicated contributor to this project. For any inquiries,
+ *     feel free to contact Michael at michaelbrockus@gmail.com.
+ * ==============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    puts("Hello, World!");
+    printf("%s", s);
+
+    return EXIT_SUCCESS;
+}
